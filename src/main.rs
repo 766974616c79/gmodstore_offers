cb8936678c30985d567556fe42353236b8b0bb0b
766974616c79 <100430077+766974616c79@users.noreply.github.com>
2026-01-11 22:04:57 +0100

    New by id

--- src/main.rs
+++ src/main.rs
@@ -22,27 +22,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         let tab = browser.new_tab()?;
         tab.navigate_to("https://www.gmodstore.com/jobmarket/jobs/browse")?;
 
-        let name = tab.wait_for_xpath(
-            "/html/body/div[1]/main/div/div[2]/div[2]/div/div[3]/div[1]/div/div[1]",
-        )?;
-
-        let url =
+        let url_element =
             tab.wait_for_xpath("/html/body/div[1]/main/div/div[2]/div[2]/div/div[3]/div[1]/div/a")?;
 
-        let old_name = read_to_string(PATH)?;
-        let new_name = name.get_inner_text()?;
-        if old_name != new_name {
+        let url = url_element.get_attribute_value("href")?.unwrap();
+        let old_url = read_to_string(PATH)?;
+        if old_url != url {
+            let name_element = tab.wait_for_xpath(
+                "/html/body/div[1]/main/div/div[2]/div[2]/div/div[3]/div[1]/div/div[1]",
+            )?;
+
+            let name = name_element.get_inner_text()?;
             Client::new()
                 .post(WEBHOOK)
                 .json(&Body {
-                    content: format!(
-                        "@everyone [**{new_name}**]({})",
-                        url.get_attribute_value("href")?.unwrap()
-                    ),
+                    content: format!("@everyone [**{name}**]({})", url),
                 })
                 .send()?;
 
-            write(PATH, new_name)?;
+            write(PATH, url)?;
         }
 
         sleep(Duration::from_secs(60));
